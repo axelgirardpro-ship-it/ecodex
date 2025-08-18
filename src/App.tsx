5408b685be41cdf382594e4ede1c98f1287987d4
Axel Girard <axelgirard@macbookpro.home>
2025-08-18 13:53:32 +0200

    feat: ajouter route auth/callback pour le système d'invitation natif Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { ImpersonationBanner } from "@/components/ui/ImpersonationBanner";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import AuthCallback from "./pages/AuthCallback";
 
 import { SearchDashboard } from "@/components/search/algolia/AlgoliaSearchDashboard";
 import { FavorisAlgoliaDashboard } from "@/components/search/favoris/FavorisAlgoliaDashboard";
@@ -84,6 +85,7 @@ const App = () => (
                     <Route path="/" element={<Index />} />
                     <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
                     <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
+                    <Route path="/auth/callback" element={<AuthCallback />} />
                     <Route path="/dev" element={<PublicRoute><DevLogin /></PublicRoute>} />
                     <Route path="/search" element={<ProtectedRoute><SearchDashboard /></ProtectedRoute>} />
                     <Route path="/favoris" element={<ProtectedRoute><FavorisAlgoliaDashboard /></ProtectedRoute>} />
